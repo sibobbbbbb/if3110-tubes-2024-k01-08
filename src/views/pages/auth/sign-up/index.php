8caf6e9eae98bb73dd1bfc6608b253cd7ce43bc5
cupski <yusufrafi77@gmail.com>
2024-10-16 23:24:41 +0700

    feat: auth/sign-up

---
+++ src/views/pages/auth/sign-up/index.php
@@ -0,0 +1,32 @@
+<main class="main">
+    <section class="signup">
+        <!-- Title -->
+        <h1 class="signup__title">Come and Join Us Now</h1>
+        <h2 class="signup__slogan">Make the most of your proffesional life</h2>
+        
+        <!-- Signup Options -->
+        <div class="signup__options">
+          <div class="signup__opt">
+            <h3 class="signup__opt-title">Are You a Job Seeker?</h3>
+            <a href="/auth/sign-up-jobseeker" class="signup__button">
+              Click here
+            </a>
+          </div>
+          
+          <div class="signup__opt">
+            <h3 class="signup__opt-title">Are You Hiring?</h3>
+            <a href="/auth/sign-up-company" class="signup__button">
+              Click here
+            </a>
+          </div>
+        </div>
+
+        <!-- Sign in -->
+        <p class="signup__signin-prompt">
+            Already have an account?
+            <a href="/auth/sign-in" class="anchor-link">
+                Sign in
+            </a>
+        </p>
+    </section>
+</main>
