a8bf3855580b671d82928b363c5e4ff4d6481b87
Yusufarsan <sandiyusufardian@gmail.com>
2024-10-22 17:45:30 +0700

    feat: added header for page apply

--- src/views/pages/jobs/[jobId]/apply/index.php
+++ src/views/pages/jobs/[jobId]/apply/index.php
@@ -1,3 +1,10 @@
+<?php
+
+use src\dao\JobType;
+use src\dao\LocationType;
+
+?>
+
 <main class="main">
     <!-- Card -->
     <section class="card">
@@ -10,6 +17,67 @@
 
             <span>Back</span>
         </a>
+
+        <div class="header__group">
+                <!-- Title -->
+                <h1 class="header__title">
+                    <?= htmlspecialchars($job->getPosition()) ?>
+                </h1>
+
+                <!-- Tags -->
+                <div class="header__tags">
+                    <!-- Job type -->
+                    <div class="badge badge--default">
+                        <?= htmlspecialchars(JobType::renderText($job->getJobType())) ?>
+                    </div>
+
+                    <!-- Location Type -->
+                    <div class="badge badge--secondary ">
+                        <?= htmlspecialchars(LocationType::renderText($job->getLocationType())) ?>
+                    </div>
+
+                </div>
+
+                <div class="header__informations">
+                    <!-- Company -->
+                    <div class="header__information">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-building-2 icon--sm--margin">
+                            <path d="M6 22V4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v18Z" />
+                            <path d="M6 12H4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h2" />
+                            <path d="M18 9h2a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2h-2" />
+                            <path d="M10 6h4" />
+                            <path d="M10 10h4" />
+                            <path d="M10 14h4" />
+                            <path d="M10 18h4" />
+                        </svg>
+
+                        <span>
+                            <?= htmlspecialchars($job->getCompanyDetail()->getName()) ?>
+                        </span>
+                    </div>
+
+
+                    <!-- Created at -->
+                    <div class="header__information">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-days icon--sm--margin">
+                            <path d="M8 2v4" />
+                            <path d="M16 2v4" />
+                            <rect width="18" height="18" x="3" y="4" rx="2" />
+                            <path d="M3 10h18" />
+                            <path d="M8 14h.01" />
+                            <path d="M12 14h.01" />
+                            <path d="M16 14h.01" />
+                            <path d="M8 18h.01" />
+                            <path d="M12 18h.01" />
+                            <path d="M16 18h.01" />
+                        </svg>
+
+                        <span>
+                            <?= htmlspecialchars($job->getCreatedAt()->format('m/d/Y')) ?>
+                        </span>
+                    </div>
+                </div>
+            </div>
 
         <!-- Title -->
         <h1 class="card__title">
